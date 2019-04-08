d21041e7e97cbc6037d6d1117bbda51319cb7978
yigenzhi <2908860706@qq.com>
2019-04-08 15:17:18 +0800

    tian

---
+++ src/com/bjsxt/controller/Tian.java
@@ -0,0 +1,14 @@
+package com.bjsxt.controller;
+
+/**
+ * describe:
+ *
+ * @author yanguodong
+ * @create 2019-04-08 15:16
+ */
+public class Tian {
+
+    public static void main(String[] args) {
+        System.out.println("魅力值");
+    }
+}
