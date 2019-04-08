d912f4ba8520a7b20fc551d43de68e0dd5c2a81c
yigenzhi <2908860706@qq.com>
2019-04-08 15:03:47 +0800

    Test2 main方法

---
+++ src/com/bjsxt/controller/Test2.java
@@ -0,0 +1,14 @@
+package com.bjsxt.controller;
+
+/**
+ * describe:
+ *
+ * @author yanguodong
+ * @create 2019-04-08 15:03
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+
+    }
+}
