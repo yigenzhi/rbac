0bcc7dcfd373a4f8558805f8077571541e34b0e9
yigenzhi <2908860706@qq.com>
2019-04-09 14:24:30 +0800

    去掉++

--- src/com/bjsxt/controller/Tian.java
+++ src/com/bjsxt/controller/Tian.java
@@ -9,7 +9,7 @@ package com.bjsxt.controller;
 public class Tian {
 
     public static void main(String[] args) {
-        System.out.println("魅力值+++");
+        System.out.println("魅力值");
     }
 
 }
